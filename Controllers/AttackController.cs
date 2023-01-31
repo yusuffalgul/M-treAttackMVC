115e78920cad81de8dcaab637e350e75b9cb92ab
yusuffalgul <45359117+yusuffalgul@users.noreply.github.com>
2023-01-31 21:55:52 +0300

    Add files via upload

---
+++ Controllers/AttackController.cs
@@ -0,0 +1,131 @@
+﻿using FinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls.WebParts;
+
+namespace FinalProject.Controllers
+{
+    public class AttackController : Controller
+    {
+        [HttpGet]
+        public ActionResult Attack()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Attack(string testCompanyName1016, string labelResult)
+        {
+            int dateTime;
+            int hour;
+            int miliSecond;
+            string filePath = "C:\\Users\\batuh\\OneDrive\\Masaüstü\\FinalProject\\FinalProject\\Results\\{0}_{1}_{2}_{3}.txt";
+            dateTime = DateTime.Now.Day;
+            hour = DateTime.Now.Hour;
+            miliSecond = DateTime.Now.Millisecond;
+            filePath = String.Format(filePath, testCompanyName1016, dateTime, hour, miliSecond);
+
+            var context = new WebApplicationMidtermEntities1();
+            var customer = new TestResult { CompanyName = testCompanyName1016, FilePath = filePath };
+            context.TestResult.Add(customer);
+            context.SaveChanges();
+            Process.Start("cmd.exe", "/k" + "tasklist.exe" + "&" + "sc query" + "&" + "sc query state= all" +">" + filePath);
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult T1007()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult T1007(string testCompanyName1007)
+        {
+            int dateTime;
+            int hour;
+            int miliSecond;
+            string filePath = "C:\\Users\\batuh\\OneDrive\\Masaüstü\\FinalProject\\FinalProject\\Results\\{0}_{1}_{2}_{3}.txt";
+            dateTime= DateTime.Now.Day;
+            hour = DateTime.Now.Hour;
+            miliSecond = DateTime.Now.Millisecond;
+            filePath = String.Format(filePath, testCompanyName1007,dateTime,hour,miliSecond);
+
+            var context = new WebApplicationMidtermEntities1();
+            var customer = new TestResult { CompanyName = testCompanyName1007, FilePath = filePath };
+            context.TestResult.Add(customer);
+            context.SaveChanges();
+
+            Process.Start("cmd.exe", "/k" + "ipconfig /all" + "&" + "netsh interface show interface" + "&" + "arp -a" + "&" + "nbtstat -n" + "&" + "net config" + ">" + filePath);
+            
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult TestResults()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TestResults(string testResultName,string labelResult)
+        {
+            var context = new WebApplicationMidtermEntities1();
+            var customer = (from TestResult in context.TestResult where TestResult.CompanyName == testResultName select TestResult.FilePath).ToList();
+            if(customer == null)
+            {
+                    Debug.WriteLine("Not Found");
+            }
+            else
+            {
+                for (int i = 0; i < customer.Count; i++)
+                {
+                    Debug.WriteLine(customer[i]);
+                    Debug.WriteLine(customer.Count);
+                    string path = customer[i].Split('\\').Last().ToString();
+                    Debug.WriteLine(path);
+                    FileStream fs = new FileStream(Server.MapPath("~/Results/" + path), FileMode.Open);
+                    StreamReader sr = new StreamReader(fs);
+                    labelResult = sr.ReadToEnd();
+                    if (i == 0)
+                    {
+                        TempData["Result"] = labelResult;
+                    }
+                    else if(i == 1) {
+
+                        TempData["Result2"] = labelResult;
+                    }
+                    else if (i == 2)
+                    {
+                        TempData["Result3"] = labelResult;
+                    }
+
+                }
+                
+                //foreach (var item in customer)
+                //{
+                //    Debug.WriteLine(item);
+                //    string path = item.Split('\\').Last().ToString();
+                //    Debug.WriteLine(path);
+                //    FileStream fs = new FileStream(Server.MapPath("~/Results/" + path), FileMode.Open);
+                //    StreamReader sr = new StreamReader(fs);
+                //    labelResult = sr.ReadToEnd();
+                //    TempData["Result"] = labelResult;
+     
+                //}
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ Controllers/DatabaseController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using FinalProject.Models;
+
+namespace FinalProject.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        WebApplicationMidtermEntities1 db = new WebApplicationMidtermEntities1();
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string name, string password)
+        {
+            var context = new WebApplicationMidtermEntities1();
+            
+            var customer = (from Customer in context.Customer
+                            where Customer.CompanyName == name && Customer.Password == password select Customer).FirstOrDefault();
+            if(customer == null)
+            {
+                return View();
+
+            } 
+            else
+            {
+                return RedirectToAction("Home","Home");
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult SignUp() {
+             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(Customer customer)
+        {
+            db.Customer.Add(customer);
+            db.SaveChanges();
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet]
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Profile(string TxtUpdateCompanyName,string TxtUpdatePhoneNumber,string TxtUpdateContactName,string TxtUpdateEmail)
+        {
+            var context = new WebApplicationMidtermEntities1();
+            var customer = context.Customer.Where(c => c.CompanyName == TxtUpdateCompanyName).FirstOrDefault();
+            if(customer != null) {
+                customer.ContactName = TxtUpdateContactName;
+                customer.PhoneNumber = TxtUpdatePhoneNumber;
+                customer.Email = TxtUpdateEmail;
+                context.SaveChanges();
+            }
+
+            return View();
+        }
+    }
+}
